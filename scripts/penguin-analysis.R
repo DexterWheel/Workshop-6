2f4ce4e6b9f515d75de3709d62ab4278b59dd582
DexterWheel <dw1182@york.ac.uk>
2020-11-26 21:49:04 +0000

    h

---
+++ scripts/penguin-analysis.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(GGally)
+library(caret)
+
+library(tidyr)
+library(dplyr)
+library(palmerpenguins)
+
+#In these slides show you will apply:
+  
+#LDA to the Penguin data without training and testing
+
+#LDA to the Penguin data with training and testing
+
+#LDA to the scRNASeq data with training and testing but we will consider how good the model is at predicting classes from the training set compared to the test set.
+
+
